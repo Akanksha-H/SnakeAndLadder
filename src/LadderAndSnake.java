962aa1d39e18e7383337c2e2366d21c326e4c881
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 23:53:57 +0530

    Added_Options

--- src/LadderAndSnake.java
+++ src/LadderAndSnake.java
@@ -1,12 +1,26 @@
 public class LadderAndSnake {
+    static final int LADDER = 0;
+    static final int SNAKE = 1;
+
     public static void main(String[] args) {
         System.out.println("WELCOME_TO_THE_GAME");
-        int diceRoll = (int) (Math.round(Math.random() * 10) % 6 + 1);
-        String playerName = "Name = Annie";
-        String playerId = "ID = ID562353";
+        int diceRoll = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        int option = (int) (Math.floor(Math.random() * 10) % 3);
+        int position = 0;
 
-        System.out.println(playerName);
-        System.out.println(playerId);
-        System.out.println(diceRoll);
+        switch (option) {
+            case SNAKE:
+                position -= diceRoll;
+                System.out.println("You have got the snake, position = " + position);
+                break;
+
+            case LADDER:
+                position += diceRoll;
+                System.out.println("You have got the ladder, position  = " + position);
+                break;
+
+            default:
+                System.out.println("You are at the same position = " + position);
+        }
     }
 }
