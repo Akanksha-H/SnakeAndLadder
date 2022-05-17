ece24c51c3a884306df9e6e197a75d8b662c63f6
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 23:44:30 +0530

    Added_Rolling_Dice

--- src/LadderAndSnake.java
+++ src/LadderAndSnake.java
@@ -1,12 +1,12 @@
 public class LadderAndSnake {
     public static void main(String[] args) {
         System.out.println("WELCOME_TO_THE_GAME");
-        int start = 0;
+        int diceRoll = (int) (Math.round(Math.random() * 10) % 6 + 1);
         String playerName = "Name = Annie";
         String playerId = "ID = ID562353";
 
         System.out.println(playerName);
         System.out.println(playerId);
-        System.out.println("start point = " + start);
+        System.out.println(diceRoll);
     }
 }
