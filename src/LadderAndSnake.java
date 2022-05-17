30a1ff7e8e2d2f9aab6ea3980988aa7746b8feb4
Aakanksha <aakanksha0499@gmail.com>
2022-05-17 23:38:30 +0530

    Added_Player_Position-0

--- src/LadderAndSnake.java
+++ src/LadderAndSnake.java
@@ -1,5 +1,12 @@
 public class LadderAndSnake {
     public static void main(String[] args) {
         System.out.println("WELCOME_TO_THE_GAME");
+        int start = 0;
+        String playerName = "Name = Annie";
+        String playerId = "ID = ID562353";
+
+        System.out.println(playerName);
+        System.out.println(playerId);
+        System.out.println("start point = " + start);
     }
 }
